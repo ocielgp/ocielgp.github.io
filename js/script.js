ce314fcdb90b9ea12391a00330fd4549ad78d26c
ocielgp <ocielgp@outlook.com>
2022-01-16 21:57:17 -0600

    2022 update

---
+++ js/script.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+  const aboutTextList = [
+    'PROGRAMADOR',
+    'DESARROLLADOR WEB',
+    'ARMADO DE COMPUTADORAS',
+    'INGENIERO EN SISTEMAS COMPUTACIONALES',
+  ]
+  let letterCount = 0;
+  let currentText = 0;
+  const aboutText = document.getElementById('about-text');
+
+  function incrementAboutText() {
+    if (letterCount < aboutTextList[currentText].length) {
+      aboutText.innerHTML += aboutTextList[currentText].charAt(letterCount);
+      letterCount++;
+      setTimeout(incrementAboutText, 100);
+    } else {
+      setTimeout(decrementAboutText, 1000);
+    }
+  }
+
+  function decrementAboutText() {
+    if (letterCount > 0) {
+      aboutText.innerHTML = aboutText.innerHTML.slice(0, -1);
+      letterCount--;
+      setTimeout(decrementAboutText, 50);
+    } else {
+      if (currentText === aboutTextList.length - 1) {
+        currentText = 0;
+      } else {
+        currentText++;
+      }
+      incrementAboutText();
+    }
+  }
+  incrementAboutText();
+
+  const year = document.getElementById('year');
+  year.innerHTML = new Date().getFullYear();
+
+
+})();
